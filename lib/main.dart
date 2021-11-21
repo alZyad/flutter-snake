cc81eea88d1a8cef6377a29287636147f0e2c4c6
2019alalguiz <zyad.alalgui@student-cs.com>
2021-11-22 00:01:46 +0100

    cleared bugs, game logic works

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -54,14 +56,20 @@ class _MyHomePageState extends State<MyHomePage> {
   late String newDirection = "down";  
   late int food = randomFood(totalNumberOfCubes);
   late bool isPlaying = false;
-  late bool isPaused;
+  late bool isPaused = false;
+  late int score = 0;
+  late int highestScore = 0;
+  late int turns = 0;
 
   void initGame() {
     snakePosition = [0, 20, 40, 60, 80];
+    newDirection = 'down';
     direction = 'down';
     food = randomFood(totalNumberOfCubes);
     isPlaying = false;
     isPaused = false;
+    score = 0;
+    turns = 0;
   }
 
   Future<void> startGame() async {
@@ -77,9 +85,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void gameOver() {
+    print({"gameOver", snakePosition, "direction", direction, "newDirection", newDirection});
     setState(() {
       initGame();
     });
+    print({"gameOver over", snakePosition, "direction", direction, "newDirection", newDirection});
   }
 
   void moveSnake(snakePosition, direction) {
@@ -113,10 +123,38 @@ class _MyHomePageState extends State<MyHomePage> {
         return 'down';
     }
   }
+
+  bool checkHitWall(snakeHead, direction) {
+    print({"checkHitWall method: ", snakePosition, "direction", direction, "newDirection", newDirection});
+    bool hitWall;
+    switch (direction) {
+      case 'up':
+        hitWall = snakeHead < numberOfCubesInWidth ? true : false;
+        return hitWall;
+      case 'down':
+        hitWall = snakeHead > (totalNumberOfCubes-numberOfCubesInWidth) ? true : false;
+        return hitWall;
+      case 'left':
+        hitWall = (snakeHead % numberOfCubesInWidth) == 0 ? true : false;
+        return hitWall;
+      case 'right':
+        hitWall = (snakeHead % numberOfCubesInWidth) == (numberOfCubesInWidth - 1) ? true : false;
+        return hitWall;
+      default:
+        hitWall = false;
+        return hitWall;
+    }
+  }
+
   void updateSnake(round) {
-    final tail = snakePosition[snakePosition.length - 1];
-    // ignore: avoid_print
-    // print(snakePosition);
+    print({snakePosition, "direction", direction, "newDirection: ", newDirection});
+    final tail = snakePosition[0];
+    // Hit Top Wall
+    if(checkHitWall(snakePosition.last, newDirection)){
+      print({"we've hit the wall", checkHitWall(snakePosition.last, newDirection), newDirection});
+      gameOver();
+      return;
+    }
     setState(() {
       if(direction==oppositeDirection(newDirection)){
         moveSnake(snakePosition, direction);
@@ -124,175 +162,171 @@ class _MyHomePageState extends State<MyHomePage> {
         moveSnake(snakePosition, newDirection);
         direction = newDirection;
       }
+      turns += 1;
+      score = turns + snakePosition.length * 10;
+      score > highestScore ? highestScore = score : null;
     });
     if (snakePosition.contains(food)) {
       snakePosition.insert(0, tail);
       food = randomFood(totalNumberOfCubes);
     }
     bool biteTail = (snakePosition.length - snakePosition.toSet().length) > 0;
-    bool hitWall = false;
-    for (var part in snakePosition) {
-      hitWall = part < 1 || part > totalNumberOfCubes;
-      if (hitWall) {
-        break;
-      }
-    }
-    print(round);
-    print(direction);
-    print(snakePosition);
-    print(biteTail);
     if (biteTail) {
       gameOver();
+      return;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: GestureDetector(
-        onVerticalDragUpdate: (detail) {
-          print({"vertical",detail.delta});
-          if (detail.delta.dy > 0) {
-            newDirection = 'down';
-          } else if (detail.delta.dy < 0) {
-            newDirection = 'up';
-          }
-        },
-        onHorizontalDragUpdate: (detail) {
-          print({"horizontal",detail.delta});
-          if (detail.delta.dx > 0) {
-            newDirection = 'right';
-          } else if (detail.delta.dx < 0) {
-            newDirection = 'left';
-          }
-          print(direction);
-        },
-        child: Scaffold(
-          backgroundColor: Colors.black,
-          body: Column(
-            children: [
-              GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: numberOfCubesInWidth,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  final colorGrid = Colors.grey[900];
-                  const colorSnake = Colors.white;
-                  const colorFood = Colors.green;
-                  var couleur = (snakePosition.contains(index))
-                      ? colorSnake
-                      : (index == food ? colorFood : colorGrid);
-                  return Container(
-                    width: cubeSize,
-                    height: cubeSize,
-                    padding: const EdgeInsets.all(2),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(5),
-                      child: Container(color: couleur),
-                    ),
-                  );
-                },
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: totalNumberOfCubes,
-                shrinkWrap: true,
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Expanded(
-                      child: Text(
-                        'Highest score',
-                        style: TextStyle(color: Colors.white),
-                        textAlign: TextAlign.center,
+    return Container(
+      color: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GestureDetector(
+          onVerticalDragUpdate: (detail) {
+            if (detail.delta.dy > 0 && direction != 'up') {
+              newDirection = 'down';
+            } else if (detail.delta.dy < 0 && direction != 'down') {
+              newDirection = 'up';
+            }
+          },
+          onHorizontalDragUpdate: (detail) {
+            if (detail.delta.dx > 0 && direction != 'left') {
+              newDirection = 'right';
+            } else if (detail.delta.dx < 0 && direction != 'right') {
+              newDirection = 'left';
+            }
+          },
+          child: Scaffold(
+            backgroundColor: Colors.black,
+            body: Column(
+              children: [
+                GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: numberOfCubesInWidth,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    final colorGrid = Colors.grey[900];
+                    final colorSnakeHead = Colors.grey[350];
+                    const colorSnakeBody = Colors.white;
+                    const colorFood = Colors.green;
+                    var couleur = (snakePosition.contains(index))
+                        ? (index == snakePosition.last ? colorSnakeHead: colorSnakeBody)
+                        : (index == food ? colorFood : colorGrid);
+                    return Container(
+                      width: cubeSize,
+                      height: cubeSize,
+                      padding: const EdgeInsets.all(2),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Container(color: couleur),
                       ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        'Score: ',
-                        style: TextStyle(color: Colors.white),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
+                    );
+                  },
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: totalNumberOfCubes,
+                  shrinkWrap: true,
                 ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                Expanded(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            isPlaying ? Colors.red[600] : Colors.green,
-                          ),
-                          fixedSize: MaterialStateProperty.all<Size>(
-                            const Size(150, 60),
-                          ),
-                          alignment: Alignment.center,
-                        ),
-                        onPressed: () {
-                          if (!isPlaying) {
-                            setState(() {
-                              isPlaying = true;
-                            });
-                            startGame();
-                          } else {
-                            setState(() {
-                              initGame();
-                            });
-                          }
-                        },
-                        child: SizedBox(
-                          child: Text(
-                            (isPlaying ? "Stop" : "Start"),
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
+                      Expanded(
+                        child: Text(
+                          'Highest score: $highestScore',
+                          style: const TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
                         ),
                       ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all<Size>(
-                            const Size(150, 60),
-                          ),
-                          alignment: Alignment.center,
-                          backgroundColor: MaterialStateProperty.all(
-                            Colors.blue,
-                          ),
-                        ),
-                        onPressed: () {
-                          if (isPlaying) {
-                            setState(() {
-                              isPaused = !isPaused;
-                              startGame();
-                            });
-                          }
-                        },
-                        child: SizedBox(
-                          child: Text(
-                            (!isPaused ? "Pause" : "Resume"),
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
+                      Expanded(
+                        child: Text(
+                          'Score: $score',
+                          style: const TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
                         ),
                       ),
                     ],
                   ),
                 ),
-              )
-            ],
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                              isPlaying ? Colors.red[600] : Colors.green,
+                            ),
+                            fixedSize: MaterialStateProperty.all<Size>(
+                              const Size(150, 60),
+                            ),
+                            alignment: Alignment.center,
+                          ),
+                          onPressed: () {
+                            if (!isPlaying) {
+                              setState(() {
+                                isPlaying = true;
+                              });
+                              print({"starting", snakePosition, "direction:", direction, "newDirection", newDirection});
+                              startGame();
+                            } else {
+                              print({"stopping", snakePosition, "direction:", direction, "newDirection", newDirection});
+                              setState(() {
+                                initGame();
+                              });
+                            }
+                          },
+                          child: SizedBox(
+                            child: Text(
+                              (isPlaying ? "Stop" : "Start"),
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            fixedSize: MaterialStateProperty.all<Size>(
+                              const Size(150, 60),
+                            ),
+                            alignment: Alignment.center,
+                            backgroundColor: MaterialStateProperty.all(
+                              Colors.blue,
+                            ),
+                          ),
+                          onPressed: () {
+                            if (isPlaying) {
+                              setState(() {
+                                isPaused = !isPaused;
+                                startGame();
+                              });
+                            }
+                          },
+                          child: SizedBox(
+                            child: Text(
+                              (!isPaused ? "Pause" : "Resume"),
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
